f8afd6e8246f52c16831bc78ec19aed0a948d378
BastienLozach <bastien.lozach@gmail.com>
2017-12-04 11:19:48 +0100

    first commit

---
+++ src/main/java/insta/fr/App.java
@@ -0,0 +1,13 @@
+package insta.fr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
